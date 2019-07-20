c4d599991549c06e849a80d8074eb04671a8acd1
André Biegel <andre.biegel@icloud.com>
2019-07-21 00:03:45 +0200

    diagram generation fixed

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$1" != "" ]; then
-    docker run -v $(pwd)/diagrams:/home ryutah/plantuml-cli $1
+    docker run -v $(pwd):/home ryutah/plantuml-cli $1
 else
     echo "parameter is empty"
 fi

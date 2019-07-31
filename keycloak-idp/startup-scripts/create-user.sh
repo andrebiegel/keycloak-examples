7978fba807c3e0ac20ecbc619a5abc78cb4c4576
André Biegel <andre.biegel@icloud.com>
2019-07-31 22:41:08 +0200

    trying to use cli script

--- keycloak-idp/startup-scripts/create-user.sh
+++
@@ -1,4 +0,0 @@
-export PATH=$PATH:$JBOSS_HOME/bin
-#kcadm.sh config credentials --server http://localhost:8080/auth --realm shibboleth-realm --user admin --password password
-kcadm.sh create users -r shibboleth-realm -s username=test -s enabled=true -s email=test@example.com --no-config --server http://localhost:8080/auth --realm master --user admin --password password
-kcadm.sh set-password -r shibboleth-realm --username test --new-password test --no-config --server http://localhost:8080/auth --realm master --user admin --password password
